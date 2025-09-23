33a9d12c1a7391e4803ab8a1fc38392d8cf7fe48
Kieran Dodson <kieran.dodson@complexus.uk>
2025-09-23 15:12:48 +0100

    IO40 MAF

--- src/main/java/org/traccar/protocol/TeltonikaProtocolDecoder.java
+++ src/main/java/org/traccar/protocol/TeltonikaProtocolDecoder.java
@@ -249,6 +249,7 @@ public class TeltonikaProtocolDecoder extends BaseProtocolDecoder {
         register(32, fmbXXX, (p, b) -> p.set(Position.KEY_COOLANT_TEMP, b.readByte()));
         register(36, fmbXXX, (p, b) -> p.set(Position.KEY_RPM, b.readUnsignedShort()));
         register(39, fmbXXX, (p, b)-> p.set("intakeAirTemp", b.readByte()));
+        register(40, fmbXXX, (p, b)-> p.set("maf", b.readUnsignedShort()));
         register(43, fmbXXX, (p, b) -> p.set("milDistance", b.readUnsignedShort()));
         register(57, fmbXXX, (p, b) -> p.set("hybridBatteryLevel", b.readByte()));
         register(66, null, (p, b) -> p.set(Position.KEY_POWER, b.readUnsignedShort() * 0.001));
